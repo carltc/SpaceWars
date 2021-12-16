84872b5982d551f6415ece55abdbd09535b6f534
Carl Chittenden <33958394+carltc@users.noreply.github.com>
2021-12-16 09:43:22 +0000

    [ADD] Adding Source and Plugin files

---
+++ Source/SpaceWars/MyTestClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SPACEWARS_API MyTestClass
+{
+public:
+	MyTestClass();
+	~MyTestClass();
+};
